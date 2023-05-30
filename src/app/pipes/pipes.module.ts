0c76ab8e06b97bf143f159462a0ce9c1c2c56035
zhouxk1204 <346960620@qq.com>
2023-05-30 14:37:26 +0800

    delete pipes

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { GenderPipe } from "./gender.pipe";
-import { RolePipe } from "./role.pipe";
 
 @NgModule({
-  declarations: [GenderPipe, RolePipe],
+  declarations: [],
   imports: [CommonModule],
-  exports: [GenderPipe, RolePipe],
+  exports: [],
 })
 export class PipesModule {}
